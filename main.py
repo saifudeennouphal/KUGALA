ac950082dbf06743c7eb5cb926a7b80ae44064e2
Ijas7 <82671424+Ijas7@users.noreply.github.com>
2022-08-23 21:02:35 +0530

    removing tab3

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ else:
 
 
 	
-	tab1, tab2,tab3 = st.tabs(["Event Wise Result","Leader Board","Registration"])
+	tab1, tab2 = st.tabs(["Event Wise Result","Leader Board"])
 
 	df=athletics()
 	evn=df['Event'].unique()
@@ -117,47 +117,7 @@ else:
 		
 
 
-	with tab3:
-
-		#name,mob,mail= None
-		with st.form("reg", clear_on_submit=True):
-			tb1,tb2=st.columns(2)
-		
-			#st.write('name')
-			with tb1:
-				name=st.text_input(label='Name')
-				mob=st.text_input(label='Phone')
-				mail=st.text_input(label='E-mail')
-			with tb2:
-				gen=st.selectbox('Gender',('Male','Female','Transgender'))
-				d = st.date_input( "Date of birt",datetime.date(1999, 1, 1))
-				
-				fac=st.selectbox('Faculty',
-				('Applied Science and Technology','Arts, Education & Music','IMk, Commerce & Law','OrientalStudies','Science','Social Science'))
-			
-			items = st.multiselect(
-		 'Events (Maximum 3 events)',
-		 ['100 meter','200 meter','400 meter','1500 meter','Walking (3000 meter)','Shot put','Discus Throw','Javelin throw','Cricket Ball Throw'])
-			
-			
-			submit_button = st.form_submit_button(label='Submit')
-		#st.write(len(name),len(mob),len(mail))
-		
-		
-			
-		if len(items)>3 and submit_button==True:
-			st.error('Error: items cannot be more than 3')
-		#st.write(name,mob,mail,fac)
-		elif submit_button==True and ((len(name) and len(mob) and len(mail))<1)  :
-			st.error("Error: all fields must be filled")
-			
-		elif submit_button==True:
-			st.success(str(name)+' Successfully registered for ' + str(items))
-			
-			for event in items:
-				
-				lst=[name,mob,mail,gen,fac,str(d),event]
-				registration(lst)
+	
 				
 with st.sidebar.expander("Developers"):
  	st.markdown('#### [Prabin Raj K P](https://www.linkedin.com/in/prabinrajkp18/)')
