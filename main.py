1d57824c6ebfe6d3c90787903592d552027097ff
Ijas7 <82671424+Ijas7@users.noreply.github.com>
2022-08-23 20:41:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -57,8 +57,7 @@ else:
 	
 
 
-	tab1, tab2,tab3 = st.tabs(["Event Wise Result","Leader Board","Registration"])
-
+	
 	df=athletics()
 	evn=df['Event'].unique()
 
@@ -97,67 +96,6 @@ else:
 			
 
 		
-	with tab2:
-		c1,c2=st.columns(2)
-		
-		with c1:
-		
-			
-			st.markdown('###### Leaderboard - Faculty wise')
-			st.dataframe(df.groupby(['Faculty']).sum()['Points'].reset_index().sort_values(by='Points', ascending=False))
-		
-		with c2:
-			st.markdown('###### Leaderboard - Department wise')
-			dpt=df.groupby(['Department']).sum()['Points'].reset_index().sort_values(by='Points', ascending=False)
-			dpt=dpt.head(10)
-			st.dataframe(dpt[dpt['Department']!=''])
-
-		
-		
-
-
-	with tab3:
-
-		#name,mob,mail= None
-		with st.form("reg", clear_on_submit=True):
-			tb1,tb2=st.columns(2)
-		
-			#st.write('name')
-			with tb1:
-				name=st.text_input(label='Name')
-				mob=st.text_input(label='Phone')
-				mail=st.text_input(label='E-mail')
-			with tb2:
-				gen=st.selectbox('Gender',('Male','Female','Transgender'))
-				d = st.date_input( "Date of birt",datetime.date(1999, 1, 1))
-				
-				fac=st.selectbox('Faculty',
-				('Applied Science and Technology','Arts, Education & Music','IMk, Commerce & Law','OrientalStudies','Science','Social Science'))
-			
-			items = st.multiselect(
-		 'Events (Maximum 3 events)',
-		 ['100 meter','200 meter','400 meter','1500 meter','Walking (3000 meter)','Shot put','Discus Throw','Javelin throw','Cricket Ball Throw'])
-			
-			
-			submit_button = st.form_submit_button(label='Submit')
-		#st.write(len(name),len(mob),len(mail))
-		
-		
-			
-		if len(items)>3 and submit_button==True:
-			st.error('Error: items cannot be more than 3')
-		#st.write(name,mob,mail,fac)
-		elif submit_button==True and ((len(name) and len(mob) and len(mail))<1)  :
-			st.error("Error: all fields must be filled")
-			
-		elif submit_button==True:
-			st.success(str(name)+' Successfully registered for ' + str(items))
-			
-			for event in items:
-				
-				lst=[name,mob,mail,gen,fac,str(d),event]
-				registration(lst)
-				
 with st.sidebar.expander("Developers"):
  	st.markdown('#### [Prabin Raj K P](https://www.linkedin.com/in/prabinrajkp18/)')
  	st.markdown('#### [Vijay V Venkitesh](https://www.linkedin.com/in/vijay-v-venkitesh-673177204/)')
