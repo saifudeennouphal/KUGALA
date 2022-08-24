2cfcc6cab204f119d362d4078304fdaf02e8f365
onam2k22 <111987710+onam2k22@users.noreply.github.com>
2022-08-24 21:18:30 +0530

    comment2

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ with st.sidebar.expander("Developers"):
  	#st.markdown('#### [Mohammed Ijas S](https://www.instagram.com/ijas.21/)')
 	st.markdown('#### [Prabin Raj K P](https://www.instagram.com/prabinraj.kp/)')
 	st.markdown('#### [Vijay Venkitesh](https://www.instagram.com/vijay_v_venkitesh/)')
- 	st.write('##### Department of Futures Studies')
+ 	#st.write('##### Department of Futures Studies')
  	
      #st.image("https://static.streamlit.io/examples/dice.jpg")
 			
