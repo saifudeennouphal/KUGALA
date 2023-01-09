dea28d2cc81566f6804de69d6c22e37a8a2ad301
onam2k22 <111987710+onam2k22@users.noreply.github.com>
2023-01-09 21:06:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-     page_title="Campus olympics",
+     page_title="Campus Gala",
      page_icon="ð§",
      layout="wide",
      initial_sidebar_state="expanded",
