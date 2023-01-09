2c8b205633509560b6eed98a861724014a41f77e
saifudeennouphal <105977954+saifudeennouphal@users.noreply.github.com>
2023-01-10 01:33:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,8 +40,8 @@ st.markdown('#### Departmets Union | Researchers Union ')
 def register():
   with st.form("myform1"):
      
-     event_name= ['SELECT','Cultural','Sports','Exhibiton']
-     departments= ['', 'Department of Aquatic Biology and Fisheries', 'Department of Biochemistry','Department of Botany',
+    event_name= ['SELECT','Cultural','Sports','Exhibiton']
+    departments= ['', 'Department of Aquatic Biology and Fisheries', 'Department of Biochemistry','Department of Botany',
                 'Department of Chemistry', 'Department of Demography', 'Department of Geology', 'Department of Mathematics',
                 'Department of Physics', 'Department of Psychology' , 'Department of Statistics' , 'Department of Zoology',
                 'Department of Arabic', 'Department of Hindi', 'Department of Kerala Studies', 'Department of Linguistics',
@@ -53,11 +53,11 @@ def register():
                 'Department of Archaeology', 'Department of Economics', 'Department of History', 'Department of Islamic and West Asian Studies',
                 'Department of Political Science','Department of Sociology', 'Department of Commerce', 'Department of Education',
                 'Department of Music', 'Department of Law']
-     cul_prog = ['SELECT', 'Solo Dance','Group Dance', 'Classical Dance' ,'Song(Solo)', 'Song(Group)', 'Classical Music','Musical Instruments', 'Fashion Show']
-     spo_prog = ['SELECT', 'Carroms(Doubles)', '5s Football', 'Badminton(Mixed Doubles)']
-     exi_prog = ['SELECT', 'Artwork Exhibition', 'Department Exhibition']
-     art_exhi = ['SELECT', 'Craft', 'Spot caricature', 'Bottle Art' , 'Others'] 
-     slct_evnt= st.selectbox('Select your Event',event_name)
+    cul_prog = ['SELECT', 'Solo Dance','Group Dance', 'Classical Dance' ,'Song(Solo)', 'Song(Group)', 'Classical Music','Musical Instruments', 'Fashion Show']
+    spo_prog = ['SELECT', 'Carroms(Doubles)', '5s Football', 'Badminton(Mixed Doubles)']
+    exi_prog = ['SELECT', 'Artwork Exhibition', 'Department Exhibition']
+    art_exhi = ['SELECT', 'Craft', 'Spot caricature', 'Bottle Art' , 'Others'] 
+    slct_evnt= st.selectbox('Select your Event',event_name)
      
     if slct_evnt == "Cultural":
        cprogram = st.selectbox('Select your Programme ',cul_prog)
